cd194cee2ce989717402e7142d529d1127c97621
pativilaka <p.vilaka@gmail.com>
2023-08-14 16:12:30 -0300

    Add Post Product

---
+++ src/main/java/com/vilaka/saveentityassociationmanytomany/dto/CategoryDTO.java
@@ -0,0 +1,26 @@
+package com.vilaka.saveentityassociationmanytomany.dto;
+
+import com.vilaka.saveentityassociationmanytomany.entities.Category;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
