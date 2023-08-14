334b58054183e29cb075d0ebda5b73a83d89e607
pativilaka <p.vilaka@gmail.com>
2023-08-14 11:39:52 -0300

    Add project config

---
+++ src/main/java/com/vilaka/saveentityassociationmanytomany/SaveEntityAssociationManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.vilaka.saveentityassociationmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveEntityAssociationManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveEntityAssociationManyToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilaka/saveentityassociationmanytomany/SaveEntityAssociationManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilaka.saveentityassociationmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveEntityAssociationManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
